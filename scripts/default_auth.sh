2d9b71f8a64dc420af01c192e76226d42ac1f0b7
Jasper Dansercoer <jasper@codictive.be>
2024-04-02 20:58:31 +0200

    Failing default auth

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -4,9 +4,9 @@ pushd $(dirname "$0")/..
 
 export RPC_URL="http://localhost:5050";
 
-export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "dojo_starter::systems::actions::actions" ).address')
+export ACTIONS_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.contracts[] | select(.name == "dojo_starter::systems::actions::actions" ).address')
 
 echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS 
@@ -18,8 +18,9 @@ echo "--------------------------------------------------------------------------
 COMPONENTS=("Position" "Moves" )
 
 for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo auth grant writer $component,$ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
     # time out for 1 second to avoid rate limiting
+
     sleep 1
 done
 
